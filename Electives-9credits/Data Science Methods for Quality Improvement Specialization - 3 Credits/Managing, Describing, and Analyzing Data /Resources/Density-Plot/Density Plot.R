8ad12f62e32f808a967ea763b874c5cfa305957a
Surender N/A Kannah <62089620+skannah@users.noreply.github.com>
2024-07-09 12:32:07 -0500

    Delete Electives-9credits directory

--- Electives-9credits/Data Science Methods for Quality Improvement Specialization - 3 Credits/Managing, Describing, and Analyzing Data /Resources/Density-Plot/Density Plot.R
+++
@@ -1,8 +0,0 @@
-hist.grouped(castings$weight, xlab="Weight", freq=F)
-lines(density(castings$weight))
-
-plot(density(castings$weight), main="Density Plot of Casting Weight", xlab="Weight")
-
-dp<-density(castings$weight)
-plot(dp, main="Density Plot of Casting Weight", xlab="Weight")
-polygon(dp, col="red", border="black")
